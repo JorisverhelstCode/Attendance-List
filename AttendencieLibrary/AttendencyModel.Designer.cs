7791704d0118a0a222cad514d13b94a0d467134a
Joris Verhelst <joris.verhelst@v8897@student.hogent.be>
2020-05-06 11:43:50 +0200

    Fixed database

--- AttendencieLibrary/AttendencyModel.Designer.cs
+++ AttendencieLibrary/AttendencyModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\FPAAY91\source\repos\VDAB\Attendance-List\AttendencieLibrary\AttendencyModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\School\Cevora\Oefeningen\Projectoef1\Code\AttendencieLibrary\AttendencyModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
